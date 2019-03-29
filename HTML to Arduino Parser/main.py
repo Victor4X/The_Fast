fe5e3519c462d223823c60bec43bbd9c2e8a601d
Victor4X <viggo00@hotmail.com>
2019-03-29 08:31:56 +0100

    Parser finished

--- HTML to Arduino Parser/main.py
+++ HTML to Arduino Parser/main.py
@@ -1,14 +1,21 @@
 
 lines = []
 
-fi = open('input.txt','r')
+fi = open('input.txt', 'r')
 for line in fi:
     lines.append(str(line))
 
-fo = open('output.txt','w')
+fo = open('output.txt', 'w')
 for line in lines:
-   print (str(line)) 
-   stripped = str(line).rstrip()
-   if stripped != "":
-       fo.write("client.println(\""+str(stripped)+"\");"+"\n")
+
+    print(str(line))
+    stripped = str(line).rstrip()
+    parsedLine = ""
+    for c in stripped:
+        if c == "\"":
+            parsedLine = parsedLine + "\\\""
+        else:
+            parsedLine = parsedLine + c
+    if parsedLine != "":
+        fo.write("client.println(\""+str(parsedLine)+"\");"+"\n")
 fo.close()
